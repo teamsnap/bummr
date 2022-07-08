50e22fd0a131390af46bbec0a7d6ed1e1d6c271e
Tim <tim@teamsnap.com>
2022-07-08 10:35:34 -0500

    feat: [ISH-679] recursive bisect

--- lib/bummr/bisecter.rb
+++ lib/bummr/bisecter.rb
@@ -2,32 +2,50 @@ module Bummr
   class Bisecter
     include Singleton
 
-    def bisect
-      puts "Bad commits found! Bisecting...".color(:red)
+    MAX_BISECTS=100
 
+    def bisect
       stop_tag = Bummr::Check.instance.stop_tag
+      start_tag = Bummr::Check.instance.start_tag
+
       system("git tag #{stop_tag}")
       puts "Created tag #{stop_tag}".color(:green)
 
       system(Bummr::Language.install_dependencies_command)
       system("git bisect start")
       system("git bisect bad")
-      system("git bisect good #{Bummr::Check.instance.start_tag}")
+      system("git bisect good #{start_tag}")
 
-      Open3.popen2e("git bisect run #{Bummr::Language.bisect_command}") do |_std_in, std_out_err|
-        while line = std_out_err.gets
-          puts line
 
-          sha_regex = Regexp::new("(.*) is the first bad commit\n").match(line)
-          unless sha_regex.nil?
-            sha = sha_regex[1]
-          end
+      MAX_BISECTS.times do
+        puts "Bad commits found! Bisecting...".color(:red)
 
-          if line == "bisect run success\n"
-            puts "RUN SUCCESS?"
-            Bummr::Remover.instance.remove_commit(sha)
+        sha = nil
+
+        Open3.popen2e("git bisect run #{Bummr::Language.bisect_command}") do |_std_in, std_out_err|
+          while line = std_out_err.gets
+            puts line
+
+            sha_regex = Regexp::new("(.*) is the first bad commit\n").match(line)
+            unless sha_regex.nil?
+              sha = sha_regex[1]
+            end
+
+            if line == "bisect run success\n"
+              if RECURSIVE_BISECT
+                system("git checkout #{stop_tag}")
+                system("git tag -D #{stop_tag}")
+                system("git rebase --onto #{sha}^ #{sha}") #pluck out that commit
+                puts "Plucked out commit #{sha}".color(:red)
+                system("git tag #{stop_tag}")
+              else
+                Bummr::Remover.instance.remove_commit(sha)
+              end
+            end
           end
         end
+
+        return unless sha or RECURSIVE_BISECT
       end
     end
   end
